74622ff5710d15132f504dbf2b546f4a3ed88814
ItokianaLuc <randtoky9@gmail.com>
2023-11-19 18:16:53 +0300

    List UE Sesamien HomePage

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UeService } from '../service/ue.service';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
+
+  tableauObjects = [
+    { "name": "Note CC EC1" },
+    { "name": "Note CC EC2" },
+    { "name": "Note CC EC3" }
+  ];
+
+  listUe: any;
+
+  constructor(private ueService: UeService) { }
+
+  ngOnInit(): void {
+    this.getUeList();
+  }
+
+  getUeList() {
+    this.ueService.getListUe().subscribe(
+      (data: any) => {
+        this.listUe = data;
+        console.log(data);
+      });
+    this.tableauObjects = this.listUe;
+  }
 
 }
